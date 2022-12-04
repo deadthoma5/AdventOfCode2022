536fd5e4def67b790993969cebafb64c1fccfe08
deadthoma5 <tom.l.tran@gmail.com>
2022-12-03 23:52:05 -0600

    moved range() to reusable tools module

--- 04/04.ts
+++ 04/04.ts
@@ -1,5 +1,6 @@
 import { Console } from "console"
 import { readFileSync } from "fs"
+import * as tools from "./tools"
 
 // Parse input
 const fileName: string = './04/input'
@@ -11,8 +12,8 @@ var part1 = 0
 assignments.forEach(function(assignment) {
     const a = assignment.split(',')[0].split('-').map(x => parseInt(x))
     const b = assignment.split(',')[1].split('-').map(x => parseInt(x))
-    const arange = range(a[0], a[1])
-    const brange = range(b[0], b[1])
+    const arange = tools.range(a[0], a[1])
+    const brange = tools.range(b[0], b[1])
     const isSubset = brange.every(function(val) { return arange.indexOf(val) >= 0 }) || arange.every(function(val) { return brange.indexOf(val) >= 0 })
     if (isSubset)
         part1++
@@ -24,15 +25,10 @@ var part2 = 0
 assignments.forEach(function(assignment) {
     const a = assignment.split(',')[0].split('-').map(x => parseInt(x))
     const b = assignment.split(',')[1].split('-').map(x => parseInt(x))
-    const arange = range(a[0], a[1])
-    const brange = range(b[0], b[1])
+    const arange = tools.range(a[0], a[1])
+    const brange = tools.range(b[0], b[1])
     const hasOverlap = brange.some(function(val) { return arange.indexOf(val) >= 0 }) || arange.some(function(val) { return brange.indexOf(val) >= 0 })
     if (hasOverlap)
         part2++
 })
 console.log("Part 2:", part2)
-
-// Function range() returns an array of the range between two numbers
-function range(start, end) {
-    return Array.from(Array(end - start + 1).keys()).map(x => x + start)
-}
