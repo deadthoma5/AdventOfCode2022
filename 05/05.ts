b3786e15bd9de5e7fa1d8af60bcd36787d2f325e
deadthoma5 <tom.l.tran@gmail.com>
2022-12-05 00:28:14 -0600

    Completed Day 05

---
+++ 05/05.ts
@@ -0,0 +1,86 @@
+import { Console } from "console"
+import { readFileSync } from "fs"
+import * as tools from "./tools"
+
+// Parse input
+const fileName: string = './05/input'
+const lines: string = readFileSync(fileName, 'utf-8')
+const stackCount: number = parseInt(lines.split('\n\n')[0].split('\n').reverse()[0].at(-2))
+var cratelines = lines.split('\n\n')[0].split('\n').reverse().slice(1,)
+var steps = lines.split('\n\n')[1].split('\n').map(x => x.split(' '))
+var stacks = []
+
+
+// Part 1
+var part1 = ""
+
+for (var i = 1; i <= stackCount; i++)
+    stacks[i] = []
+
+cratelines.forEach(function(crateline) {
+    let position = 0
+    for (let i = 1; i <= stackCount; i++) {
+        position = (i == 1) ? position + 1 : position + 3
+
+        if (crateline.charAt(position) != ' ')
+            stacks[i].push(crateline.charAt(position))
+        
+        position++
+    }
+})
+
+steps.forEach(function(step) {
+    let count = parseInt(step[1])
+    let source = parseInt(step[3])
+    let destination = parseInt(step[5])
+    for (let i = 0; i < count; i++) {
+        let item = stacks[source].pop(-1)
+        stacks[destination].push(item)
+    }
+})
+
+for (var i = 1; i <= stackCount; i++)
+    if (stacks[i].length > 0)
+        part1 += stacks[i].at(-1)
+
+console.log("Part 1:", part1)
+
+
+// Part 2
+var part2 = ""
+
+for (var i = 1; i <= stackCount; i++)
+    stacks[i] = []
+
+cratelines.forEach(function(crateline) {
+    let position = 0
+    for (let i = 1; i <= stackCount; i++) {
+        position = (i == 1) ? position + 1 : position + 3
+
+        if (crateline.charAt(position) != ' ')
+            stacks[i].push(crateline.charAt(position))
+        
+        position++
+    }
+})
+
+steps.forEach(function(step) {
+    let count = parseInt(step[1])
+    let source = parseInt(step[3])
+    let destination = parseInt(step[5])
+    let block = []
+    for (let i = 0; i < count; i++) {
+        let item = stacks[source].pop(-1)
+        block.push(item)
+    }
+    block.reverse()
+    block.forEach(function(item) {
+        stacks[destination].push(item)
+    })
+})
+
+for (var i = 1; i <= stackCount; i++)
+    if (stacks[i].length > 0)
+        part2 += stacks[i].at(-1)
+
+console.log("Part 2:", part2)
---
+++ 05/tools.ts
@@ -0,0 +1,4 @@
+// Function range() returns an array of the range between two numbers
+export function range(start, end) {
+    return Array.from(Array(end - start + 1).keys()).map(x => parseInt(x + start))
+}
