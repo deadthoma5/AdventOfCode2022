6317ce18fd07ecff3a2af7a432a09fc88b41a7f9
deadthoma5 <tom.l.tran@gmail.com>
2022-12-05 00:36:54 -0600

    Code cleanup

--- 05/05.ts
+++ 05/05.ts
@@ -7,39 +7,37 @@ const fileName: string = './05/input'
 const lines: string = readFileSync(fileName, 'utf-8')
 const stackCount: number = parseInt(lines.split('\n\n')[0].split('\n').reverse()[0].at(-2))
 var cratelines = lines.split('\n\n')[0].split('\n').reverse().slice(1,)
-var steps = lines.split('\n\n')[1].split('\n').map(x => x.split(' '))
+var steps = lines.split('\n\n')[1].split('\n').map(x => x.split(' ').map(y => parseInt(y)))
 var stacks = []
 
 
 // Part 1
 var part1 = ""
 
-for (var i = 1; i <= stackCount; i++)
+for (let i = 1; i <= stackCount; i++)
     stacks[i] = []
 
 cratelines.forEach(function(crateline) {
     let position = 0
     for (let i = 1; i <= stackCount; i++) {
         position = (i == 1) ? position + 1 : position + 3
-
         if (crateline.charAt(position) != ' ')
             stacks[i].push(crateline.charAt(position))
-        
         position++
     }
 })
 
 steps.forEach(function(step) {
-    let count = parseInt(step[1])
-    let source = parseInt(step[3])
-    let destination = parseInt(step[5])
+    let count = step[1]
+    let source = step[3]
+    let destination = step[5]
     for (let i = 0; i < count; i++) {
         let item = stacks[source].pop(-1)
         stacks[destination].push(item)
     }
 })
 
-for (var i = 1; i <= stackCount; i++)
+for (let i = 1; i <= stackCount; i++)
     if (stacks[i].length > 0)
         part1 += stacks[i].at(-1)
 
@@ -49,25 +47,23 @@ console.log("Part 1:", part1)
 // Part 2
 var part2 = ""
 
-for (var i = 1; i <= stackCount; i++)
+for (let i = 1; i <= stackCount; i++)
     stacks[i] = []
 
 cratelines.forEach(function(crateline) {
     let position = 0
     for (let i = 1; i <= stackCount; i++) {
         position = (i == 1) ? position + 1 : position + 3
-
         if (crateline.charAt(position) != ' ')
             stacks[i].push(crateline.charAt(position))
-        
         position++
     }
 })
 
 steps.forEach(function(step) {
-    let count = parseInt(step[1])
-    let source = parseInt(step[3])
-    let destination = parseInt(step[5])
+    let count = step[1]
+    let source = step[3]
+    let destination = step[5]
     let block = []
     for (let i = 0; i < count; i++) {
         let item = stacks[source].pop(-1)
@@ -79,7 +75,7 @@ steps.forEach(function(step) {
     })
 })
 
-for (var i = 1; i <= stackCount; i++)
+for (let i = 1; i <= stackCount; i++)
     if (stacks[i].length > 0)
         part2 += stacks[i].at(-1)
 
